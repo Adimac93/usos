500c75e363a779f0e97a53dd6a7b54d4476a6086
HubertK05 <hubert.klimowicz.hk@gmail.com>
2024-08-13 13:15:11 +0200

    add parse query params util function

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::errors::AppError;
 
 pub(crate) trait ToAppResult
@@ -16,5 +18,74 @@ impl ToAppResult for reqwest::Response {
         } else {
             return Ok(self);
         }
+    }
+}
+
+pub(crate) fn parse_ampersand_params(
+    text: impl Into<String>,
+) -> Result<HashMap<String, String>, AppError> {
+    let text = text.into();
+    let mut res = HashMap::new();
+    let e = AppError::Parse(format!("Invalid ampersand params syntax: {text}"));
+
+    for keyval in text.split('&') {
+        if let Some((key, val)) = keyval.split_once('=') {
+            if val.contains('=') || key.is_empty() {
+                return Err(e);
+            }
+
+            res.insert(key.to_string(), val.to_string());
+        } else {
+            if !keyval.is_empty() {
+                return Err(e);
+            }
+        }
+    }
+
+    Ok(res)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use rstest::rstest;
+
+    use super::parse_ampersand_params;
+
+    #[rstest]
+    #[case("", &[])]
+    #[case("a=b", &[("a", "b")])]
+    #[case("abc123=abc123", &[("abc123", "abc123")])]
+    #[case("a=b&c=d", &[("a", "b"), ("c", "d")])]
+    #[case("a=b&a=c", &[("a", "c")])]
+    #[case("a=b&", &[("a", "b")])]
+    #[case("&a=b", &[("a", "b")])]
+    #[case("&a=b&c=d&", &[("a", "b"), ("c", "d")])]
+    #[case("a=b&&c=d", &[("a", "b"), ("c", "d")])]
+    #[case("a=", &[("a", "")])]
+    fn parse_ampersand_separated_params_is_successful(
+        #[case] text: &str,
+        #[case] expected: &[(&str, &str)],
+    ) {
+        let map = parse_ampersand_params(text).unwrap();
+        assert_eq!(
+            map,
+            HashMap::from_iter(
+                expected
+                    .into_iter()
+                    .map(|&x| (x.0.to_string(), x.1.to_string()))
+            )
+        )
+    }
+
+    #[rstest]
+    #[case("a==b")]
+    #[case("a=b=c&b=c")]
+    #[case("=b")]
+    #[case("abc")]
+    fn parse_ampersand_separated_params_fails(#[case] text: &str) {
+        let res = parse_ampersand_params(text);
+        assert!(res.is_err())
     }
 }
