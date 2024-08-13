69f6e9c086ba51577dee1085d9c82009e0747ec6
HubertK05 <hubert.klimowicz.hk@gmail.com>
2024-08-13 10:45:38 +0200

    add failing oauth flow tests

--- src/api/auth.rs
+++ src/api/auth.rs
@@ -17,16 +17,21 @@ use crate::{
 
 use super::scopes::Scopes;
 
-pub struct OAuthToken {
+pub struct OAuthRequestToken {
     token: String,
     secret: String,
 }
 
-pub async fn acquire_token(
+pub struct OAuthAccessToken {
+    token: String,
+    secret: String,
+}
+
+pub async fn acquire_token_old(
     consumer_key: &ConsumerKey,
     callback: Option<String>,
     scopes: Scopes,
-) -> crate::Result<OAuthToken> {
+) -> crate::Result<OAuthAccessToken> {
     let callback = callback.unwrap_or("oob".into());
     let url = UsosUri::with_path("/services/oauth/request_token");
     let authorization = authorize(
@@ -65,18 +70,7 @@ pub async fn acquire_token(
         .ok_or(AppError::usos("Invalid return param key"))?;
 
     if &callback == "oob" {
-        println!(
-            "Please visit the following URL to authorize the application: {}",
-            UsosUri::with_path(&format!(
-                "/services/oauth/authorize?oauth_token={oauth_token}"
-            ))
-        );
-
-        let mut buf = String::new();
-        print!("Enter the verifier PIN: ");
-        std::io::stdout().flush().unwrap();
-        std::io::stdin().read_line(&mut buf).unwrap();
-        let pin = buf.trim();
+        let pin: String = todo!();
 
         let url = UsosUri::with_path("/services/oauth/access_token");
         let response = CLIENT
@@ -110,27 +104,101 @@ pub async fn acquire_token(
         println!("User OAuth token: {oauth_token}");
         println!("User OAuth token secret: {oauth_token_secret}");
 
-        return Ok(OAuthToken {
+        return Ok(OAuthAccessToken {
             token: oauth_token.into(),
             secret: oauth_token_secret.into(),
         });
     }
 
-    Ok(OAuthToken {
+    Ok(OAuthAccessToken {
         token: "".into(),
         secret: "".into(),
     })
 }
 
-#[tokio::test]
-async fn test_acquire_token() {
-    dotenvy::dotenv().ok();
-    let consumer_key = ConsumerKey::from_env().unwrap();
-    acquire_token(
-        &consumer_key,
-        None,
-        Scopes::new(HashSet::from([Scope::Studies])),
-    )
-    .await
-    .unwrap();
+pub async fn acquire_request_token(
+    consumer_key: &ConsumerKey,
+    callback: Option<String>,
+    scopes: Scopes,
+) -> crate::Result<OAuthRequestToken> {
+    todo!()
+}
+
+pub async fn acquire_access_token(
+    consumer_key: &ConsumerKey,
+    request_token: String,
+    verifier: String,
+) -> crate::Result<OAuthAccessToken> {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    async fn get_pin(oauth_token: String) -> String {
+        println!(
+            "Please visit the following URL to authorize the application: {}",
+            UsosUri::with_path(&format!(
+                "/services/oauth/authorize?oauth_token={oauth_token}"
+            ))
+        );
+
+        let mut buf = String::new();
+        print!("Enter the verifier PIN: ");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut buf).unwrap();
+        let pin = buf.trim();
+
+        return pin.into();
+    }
+
+    #[tokio::test]
+    async fn acquire_request_token_is_successful() {
+        dotenvy::dotenv().ok();
+        let consumer_key = ConsumerKey::from_env().unwrap();
+        let _token = acquire_request_token(
+            &consumer_key,
+            None,
+            Scopes::new(HashSet::from([Scope::Studies])),
+        )
+        .await
+        .unwrap();
+    }
+
+    #[tokio::test]
+    async fn acquire_request_token_invalid_consumer_key() {
+        dotenvy::dotenv().ok();
+        let mut consumer_key = ConsumerKey::from_env().unwrap();
+        consumer_key.key.push('a');
+
+        let res = acquire_request_token(
+            &consumer_key,
+            None,
+            Scopes::new(HashSet::from([Scope::Studies])),
+        )
+        .await;
+
+        assert!(res.is_err());
+    }
+
+    #[tokio::test]
+    #[ignore = "requires user interaction"]
+    async fn oauth_flow_no_callback_provided() {
+        dotenvy::dotenv().ok();
+        let consumer_key = ConsumerKey::from_env().unwrap();
+        let request_token = acquire_request_token(
+            &consumer_key,
+            None,
+            Scopes::new(HashSet::from([Scope::Studies])),
+        )
+        .await
+        .unwrap();
+
+        let verifier = get_pin(request_token.token.clone()).await;
+
+        let _access_token = acquire_access_token(&consumer_key, request_token.token, verifier)
+            .await
+            .unwrap();
+    }
 }
