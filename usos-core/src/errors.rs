f57e72495c17c1d3692cf6598dd90393129cbb2d
HubertK05 <hubert.klimowicz.hk@gmail.com>
2024-09-04 10:42:15 +0200

    document AppError

--- usos-core/src/errors.rs
+++ usos-core/src/errors.rs
@@ -6,20 +6,28 @@ use crate::api::errors::UsosError;
 
 #[derive(Error, Debug)]
 pub enum AppError {
+    /// Error returned from USOS API. See [`UsosError`] and [the USOS API reference](https://apps.usos.pw.edu.pl/developers/api/definitions/errors/).
     #[error("Http error {code}")]
     Http {
         code: StatusCode,
         message: Option<UsosError>,
     },
+    /// Unexpected error caused by the crate or any of its dependencies.
     #[error(transparent)]
     Unexpected(#[from] anyhow::Error),
 }
 
 impl AppError {
+    /// Constructs an Http variant.
     pub fn http(code: StatusCode, message: Option<UsosError>) -> Self {
         Self::Http { code, message }
     }
 
+    /// Tries to extract [`UsosError`].
+    ///
+    /// In case of the `Http` variant, returns the inner [`UsosError`] if it is present.
+    ///
+    /// Calling this method on the `Unexpected` variant always results in `None`.
     pub fn usos_error(&self) -> Option<&UsosError> {
         match self {
             Self::Http { message, .. } => message.as_ref(),
